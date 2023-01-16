4e2f0be4486a32b459bbb77f9a84009464c36220
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2023-01-16 20:09:09 +0000

    Fix rubocop errors

--- author.rb
+++ author.rb
@@ -1,13 +1,13 @@
 class Author
-    attr_accessor :publisher, :cover_state
+  attr_accessor :publisher, :cover_state
 
-    def initialize(publisher, cover_state, archived: false)
-        @publisher = publisher
-        @cover_state = cover_state
-        @archived = archived
-    end
+  def initialize(publisher, cover_state, archived: false)
+    @publisher = publisher
+    @cover_state = cover_state
+    @archived = archived
+  end
 
-    def can_be_archived?
-        return ((@archived == true) || (@cover_state == 'bad')) ? true : false
-    end
+  def can_be_archived?
+    ((@archived == true) || (@cover_state == 'bad'))
+  end
 end
--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -1,25 +1,27 @@
 require './author'
 
 describe Author do
-    context 'Pass parameters through book class' do
-        new_book = Author.new('Abubakar', 'Morrison', archived: true)
-        new_book_2 = Author.new('Abubakar', 'bad')
-        new_book_3 = Author.new('Abubakar', 'Hello')
+  context 'Pass parameters through book class' do
+    new_book = Author.new('Abubakar', 'Morrison', archived: true)
+    new_book_two = Author.new('Abubakar', 'bad')
+    new_book_three = Author.new('Abubakar', 'Hello')
 
-        it 'should check for book instance' do
-            expect(new_book).to be_instance_of(Author)
-        end
-
-        it 'should check for achieve override with new_book' do
-            expect(new_book.can_be_archived?).to eql(true)
-        end
-
-        it 'should check for cover_state == bad in new_book_2' do
-            expect(new_book_2.can_be_archived?).to eql(true)
-        end
-
-        it 'should check return default false in new_book_3' do
-            expect(new_book_3.can_be_archived?).to eql(false)
-        end
+    it 'should check for book instance' do
+      expect(new_book).to be_instance_of(Author)
+      expect(new_book_two).to be_instance_of(Author)
+      expect(new_book_three).to be_instance_of(Author)
     end
+
+    it 'should check for achieve override with new_book' do
+      expect(new_book.can_be_archived?).to eql(true)
+    end
+
+    it 'should check for cover_state == bad in new_book_2' do
+      expect(new_book_two.can_be_archived?).to eql(true)
+    end
+
+    it 'should check return default false in new_book_3' do
+      expect(new_book_three.can_be_archived?).to eql(false)
+    end
+  end
 end
