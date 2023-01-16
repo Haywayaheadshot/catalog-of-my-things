8fecbb779af26c163382a95efd30a57951420a74
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2023-01-16 20:02:07 +0000

    Add book class with can_be_archived method

---
+++ book.rb
@@ -0,0 +1,13 @@
+class Book
+    attr_accessor :publisher, :cover_state
+
+    def initialize(publisher, cover_state, archived: false)
+        @publisher = publisher
+        @cover_state = cover_state
+        @archived = archived
+    end
+
+    def can_be_archived?
+        return ((@archived == true) || (@cover_state == 'bad')) ? true : false
+    end
+end
