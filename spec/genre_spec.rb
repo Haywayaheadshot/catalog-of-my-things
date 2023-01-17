71c3b3797be620bf1ea550e021bb3025db937625
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2023-01-17 20:57:49 +0000

    Add genre unit tests

---
+++ spec/genre_spec.rb
@@ -0,0 +1,19 @@
+require './genre'
+
+describe 'Check for when an object of genre is created' do
+    before :each do 
+        @new_genre = Genre.new('Pop')
+    end
+
+    it 'should for object created' do
+        expect(@new_genre).to be_instance_of(Genre)
+    end
+
+    it 'should for object name' do
+        expect(@new_genre.name).to eq('Pop')
+    end
+
+    it 'items array should be empty' do
+        expect(@new_genre.items.length).to eq(0)
+    end
+end
