1c06cb30c8ca3daa6505a71ea163655c403e4bbd
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2023-01-17 20:58:08 +0000

    Add add_item method

--- genre.rb
+++ genre.rb
@@ -1,11 +1,14 @@
-require './item'
-
 class Genre
-  attr_accessor :name, :items
+  attr_accessor :name, :items, :id
 
-  def initialize(name, _items)
+  def initialize(name)
     @id = Random.rand(1..1000)
     @name = name
     @items = []
+  end
+
+  def add_item(item)
+    @items.push(item)
+    item.genre = self unless item.genre == self
   end
 end
