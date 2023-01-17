b3693f6d766a70f4f41360e61ed21a2bb2616f37
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2023-01-17 21:52:14 +0000

    Add Label class

---
+++ label.rb
@@ -0,0 +1,14 @@
+class label
+    attr_accessor :name, :color, :id, :items
+    def initialize(name, color)
+        @id = Random(1..1000)
+        @name = name
+        @color = color
+        @items = []
+    end
+
+    def add_item(item)
+        @items.push(item)
+        item.label = self unless item.label == self
+    end
+end
