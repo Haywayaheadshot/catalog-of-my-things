1ce5c65f00612d98f018262e7aee93acca27cd9e
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2023-01-19 21:46:51 +0000

    Refactor code to check storage when listing items

--- modules/list_all_labels.rb
+++ modules/list_all_labels.rb
@@ -1,11 +1,19 @@
 module ListAllLabels
   def list_all_labels
+    stored_data = Storage.load_data('label')
+
+    puts 'Sorry! there no labels to display. Create a label first!' if stored_data.empty? && @labels_arr.empty?
+
     if @labels_arr.length >= 1
       @labels_arr.each do |label|
         puts "Label name: #{label['name']}. Label color: #{label['color']}"
       end
-    else
-      puts 'Sorry! there no labels to display. Create a label first!'
+    end
+
+    return unless stored_data.length >= 1
+
+    stored_data.map do |data|
+      puts "Label name: #{data['name']}. Label color: #{data['color']}"
     end
   end
 end
