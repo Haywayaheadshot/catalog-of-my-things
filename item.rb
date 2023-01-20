056e26509dd7b69d1230f2ddde2c45459091b62e
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2023-01-20 23:08:22 +0000

    Refactor code to match reviewer requirement

--- item.rb
+++ item.rb
@@ -1,5 +1,7 @@
+require 'date'
+
 class Item
-  attr_accessor :genre, :author, :source, :label, :publish_date, :archived
+  attr_accessor :genre, :author, :label, :publish_date, :archived
 
   def initialize(publish_date, archived: false)
     @id = Random.rand(1..1000)
@@ -7,8 +9,23 @@ class Item
     @archived = archived
   end
 
+  def genre(genre)
+    @genre = genre
+    genre.items.push(self) unless genre.items.include?(self)
+  end
+
+  def label(label)
+    @label = label
+    label.items.push(self) unless label.items.include?(self)
+  end
+
+  def author(author)
+    @author = author
+    author.items.push(self) unless author.items.include?(self)
+  end
+
   def can_be_archived?
-    @publish_date > 10
+    (Date.today - Date.parse(@publish_date)) > 10
   end
 
   def move_to_archive?
